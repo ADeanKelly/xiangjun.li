f2de464fa9b7c330abb28ce4a4a6d7fa38611547
A. Dean Kelly <a.dean.kelly@gmail.com>
2026-02-08 21:15:44 -0500

    [skip ci] Image Changes on Index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styles from "@/styles/home.module.css";
 import { getSortedPostsData } from "@/lib/md";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home({ posts }) {
   return (
@@ -8,7 +9,10 @@ export default function Home({ posts }) {
       <main className={styles.main}>
 
         {posts.map(post => (
-          <figure key={post.slug}>
+          <figure key={post.slug} style={{ position: 'relative' }}>
+            <span className="imageContainer" style={{ position: 'relative', width: '100%', paddingBottom: '56.25%', display: 'block' }}>
+              <Image src={post.thumbnail} fill='true' objectFit="cover"></Image>
+            </span>
             <figcaption>
               <Link href={post.slug}>
                 <h2>{post.title}</h2>
